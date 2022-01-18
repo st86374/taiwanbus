4bca1baa2eeeb94487785eaea9fce93e0dba1025
tom <tom@gastom.com.tw>
2022-01-18 14:50:06 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/taiwanbus/',
+  // base: '/taiwanbus/',
   plugins: [vue()],
   resolve: {
     alias: {
