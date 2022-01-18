a3f9dfc2a1f85ab408537bf8b2152f14b7002588
tom <tom@gastom.com.tw>
2022-01-18 11:38:05 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,16 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/<REPO>/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/taiwanbus/' : './',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
     },
   },
+  build: {
+    minify: true,
+  },  
   define:{
     'process.env':{
       VUE_APP_GOOGLE_MAP_API_KEY:'AIzaSyAFKPiMUc5jyVnzoWAJush-EDagXdyHgVc',
